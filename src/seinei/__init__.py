c8aecb3f3844881ccc13999fc84558a1f47aae47
Michael Christoffersen <mchristo28@gmail.com>
2023-09-08 13:47:05 -0800

    Cleaned up CLI and output, added a verbose flag

--- src/seinei/__init__.py
+++ src/seinei/__init__.py
@@ -4,14 +4,3 @@ import seinei.tilt
 import seinei.output
 
 __all__ = ["ui", "fetch", "tilt", "output"]
-
-import logging
-
-log = logging.getLogger("seinei")
-log.setLevel("INFO")
-log.propagate = 0
-ch = logging.StreamHandler()
-FORMAT = "[%(asctime)s] - %(name)s - %(levelname)s: %(message)s"
-formatter = logging.Formatter(FORMAT, datefmt="%Y-%m-%d %H:%M:%S")
-ch.setFormatter(formatter)
-log.addHandler(ch)
--- src/seinei/bin/seinei.py
+++ src/seinei/bin/seinei.py
@@ -24,16 +24,22 @@ def cli():
         help="SAC data file(s) with seismometer data in ADC counts",
     )
     parser.add_argument(
-        "-lpp",
-        "--low_pass_period",
+        "-o",
+        "--output",
         default=None,
-        help="Corner period for low-pass filter in s (default=auto-set)",
+        help="Directory to save output files (default=same as SAC files)",
     )
     parser.add_argument(
-        "-hpp",
-        "--high_pass_period",
+        "-pbl",
+        "--passband_long_period",
         default=None,
-        help="Corner period for high-pass filter in s (default=auto-set)",
+        help="Long-period edge of passband (default=auto-set)",
+    )
+    parser.add_argument(
+        "-pbs",
+        "--passband_short_period",
+        default=None,
+        help="Short-period edge of passband (default=auto-set)",
     )
     parser.add_argument(
         "-g",
@@ -41,6 +47,7 @@ def cli():
         default=9.81,
         help="Gravitaitonal acceleration in m/s^2 (default=9.81)",
     )
+    parser.add_argument('-v', '--verbose', action='store_true', help="Enable verbose output") 
     args = parser.parse_args()
 
     return args
@@ -51,6 +58,8 @@ def main():
 
     # Read StationXML inventory file, complain if obspy doesn't like it
     try:
+        if(args.verbose):
+            print("Reading inventory file:\n\t%s" % args.inventory)
         inv = obspy.core.inventory.inventory.read_inventory(
             args.inventory, format="STATIONXML", level="response"
         )
@@ -60,18 +69,26 @@ def main():
         sys.exit()
 
     # Read SAC data files
+    if(args.verbose):
+        print("Reading SAC file(s):")
     sts = {}
     for file in args.sac:
+        if(args.verbose):
+            print("\t%s" % file)
         sts[file] = obspy.read(file)
         # TODO: figure out how this complains if a response is missing and
         # use that to throw an error (or just skip the station)
         sts[file].attach_response(inv)
 
     # Loop over streams (corresponding to files)
+    if(args.verbose):
+        print("Estimating tilt:")
     for name, st in sts.items():
         tr = st[0]
+        if(args.verbose):
+            print("\t%s.%s.%s.%s - %s" % (tr.stats.network, tr.stats.station, tr.stats.location, tr.stats.channel, tr.stats.starttime))
         # Decide on low pass filter period if necessary
-        if args.low_pass_period is None:
+        if args.passband_short_period is None:
             # Generate instrument responses to translation (velocity) and tilt
             Ca, freq = tr.stats.response.get_evalresp_response(
                 t_samp=1, nfft=2 ** 13, output="ACC"
@@ -80,29 +97,28 @@ def main():
             Cv, freq = tr.stats.response.get_evalresp_response(
                 t_samp=1, nfft=2 ** 13, output="VEL"
             )
-            dbSplit = 10 * np.log10(np.abs(Ct / Cv))
-            args.low_pass_period = 1.0 / np.max(freq[np.where(dbSplit >= 20)])
+            dbSplit = 10 * np.log10(np.abs(Ct[1:] / Cv[1:]))
+            args.passband_short_period = 1.0 / np.max(freq[np.where(dbSplit >= 20)])
         else:
-            args.low_pass_period = float(args.low_pass_period)
+            args.passband_short_period = float(args.passband_short_period)
         
-        print("Low-pass period: %.2fs" % args.low_pass_period)
-
         # Decide on high pass filter period if necessary
-        if args.high_pass_period is None:
-            args.high_pass_period = tr.stats.npts / tr.stats.sampling_rate / 2
+        if args.passband_long_period is None:
+            args.passband_long_period = tr.stats.npts / tr.stats.sampling_rate / 2
         else:
-            args.high_pass_period = float(args.high_pass_period)
+            args.passband_long_period = float(args.passband_long_period)
 
-        print("High-pass period: %.2fs" % args.high_pass_period)
-
+        if(args.verbose):
+            print("\t\tPassband: %.2fs to %.2fs" % (args.passband_long_period, args.passband_short_period))
+            
         # Linear detrend
         tr.detrend(type="linear")
 
         # Filter
         tr.filter(
             type="bandpass",
-            freqmin=1.0/args.high_pass_period,
-            freqmax=1.0/args.low_pass_period,
+            freqmin=1.0/args.passband_long_period,
+            freqmax=1.0/args.passband_short_period,
             corners=4,
             zerophase=False,
         )
@@ -112,20 +128,24 @@ def main():
 
         # TODO: some smarter decimation frequency logic
 
-        if(fdec > tr.stats.sampling_rate or fdec < 2*(1/args.low_pass_period)):
-            print("Failed to find appropriate decimation frequency, exiting")
-            sys.exit()
+        if(fdec > tr.stats.sampling_rate or fdec < 2*(1/args.passband_short_period)):
+            print("\t\tFailed to find appropriate decimation frequency, skipping")
+            continue
 
         tr.decimate(int(tr.stats.sampling_rate), no_filter=True)
 
         # Get instrument response to tilt sampled at proper frequencies
         Ca, freq = tr.stats.response.get_evalresp_response(tr.stats.delta, tr.stats.npts, output="ACC")
         Ct = -np.sqrt(args.gravity)*Ca
-        DATA = np.fft.rfft(tr.data)/Ct
-        DATA[0] = 0  # Set DC to 0
+        DATA = np.fft.rfft(tr.data)[1:]/Ct[1:]
+        DATA = np.append(0, DATA)
         tr.data = np.fft.irfft(DATA)
 
-        tiltName = os.path.dirname(name) + "/tilt_" + os.path.basename(name)
+        if(args.output is None):
+            tiltName = os.path.dirname(name) + "/tilt_" + os.path.basename(name)
+        else:
+            tiltName = args.output + "/tilt_" + os.path.basename(name)
+            
         st.write(tiltName, format="SAC")
 
     return 0
